b93bdc778b8c78caeaa50fa77ed6e6374902a1d6
rallristhy <rallristhy@outlook.com>
2017-09-04 13:56:04 -0400

    Correção do app.js

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
-
 /* Importando express e criando server */
 const express = require('express');
 const server = express();
+const port = 50005;
 
 /* 
-* Toda requisição vindas para o backend passará pelo urlencoded
-* urlencoded é o formato dos dados vindos do frondend
+* Toda requisição vinda para o servidor passará pelo urlencoded
+* urlencoded é o formato dos dados vindos da view
 */
 const bodyParser = require('body-parser');
 server.use(bodyParser.urlencoded({ extended: true }));
+server.use(bodyParser.json());
+
+/* Mapeamentos de Diretórios para a View ter visibilidade */
+server.use("/node_modules",  express.static(__dirname + '/node_modules'));
+server.use("/core",  express.static(__dirname + '/core'));
+
 
 /* Rota Padrão */
 server.use ('/', function (request, response){
@@ -16,6 +22,6 @@ server.use ('/', function (request, response){
 });
 
 /* Listando APP na porta 5000 */
-server.listen (5000, function(){
-	console.log ('Server running...');
+server.listen (port, function(){
+	console.log ('Servidor em execução na porta: '+port);
 });
